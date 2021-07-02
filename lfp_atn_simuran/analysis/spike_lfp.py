2323e5b7021acd457c80b0e20c1b1607e1d8381c
Sean Martin <seankieran.m@hotmail.com>
2021-07-02 17:45:30 +0100

    Update spike lfp

--- lfp_atn_simuran/analysis/spike_lfp.py
+++ lfp_atn_simuran/analysis/spike_lfp.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 
 import simuran
 import numpy as np
+import pandas as pd
 
 from lfp_atn_simuran.analysis.lfp_clean import LFPClean
 from skm_pyutils.py_table import list_to_df
@@ -25,11 +26,13 @@ def recording_spike_lfp(recording, clean_method="avg", **kwargs):
     fmt = kwargs.get("image_format", "png")
 
     sub_sig = signals_grouped_by_region["SUB"]
+    rsc_sig = signals_grouped_by_region["RSC"]
 
     # SFC here
     nc_sig = sub_sig.to_neurochat()
+    nc_sig2 = rsc_sig.to_neurochat()
 
-    NUM_RESULTS = 2
+    NUM_RESULTS = 6
 
     output = {}
     # To avoid overwriting what has been set to analyse
@@ -64,13 +67,17 @@ def recording_spike_lfp(recording, clean_method="avg", **kwargs):
             unit.underlying.set_unit_no(cell)
             # Do analysis on that unit
             spike_train = unit.underlying.get_unit_stamp()
+
             g_data = nc_sig.plv(spike_train, mode="bs", fwin=[0, 20])
             sta = g_data["STAm"]
             sfc = g_data["SFCm"]
+            g_data = nc_sig2.plv(spike_train, mode="bs", fwin=[0, 20])
+            sta_rsc = g_data["STAm"]
+            sfc_rsc = g_data["SFCm"]
             t = g_data["t"]
             f = g_data["f"]
 
-            output[name_for_save] = [sta, sfc, t, f]
+            output[name_for_save] = [sta, sfc, sta_rsc, sfc_rsc, t, f]
             unit.underlying.reset_results()
 
     return output
@@ -92,48 +99,79 @@ def combine_results(info, extra):
     new_list1 = []
     new_list2 = []
 
-    for row in info.itertuples():
-        group = row.Directory[len(base_dir + os.sep) :][0]
-        if group == "C":
-            group = "Control"
-        elif group == "L":
-            group = "ATNx (Lesion)"
-        else:
-            raise ValueError("unsupported group {}".format(group))
-        sta = row.STA
-        sfc = row.SFC
-        t = row.Time
-        f = row.Frequency
-        for i in range(len(sta)):
-            new_list1.append([group, float(sta[i]), float(t[i])])
-        for i in range(len(sfc)):
-            new_list2.append([group, float(sfc[i]) / 100, float(f[i])])
-
-    headers1 = ["Group", "STA", "Time (s)"]
-    headers2 = ["Group", "SFC", "Frequency (Hz)"]
-    df1 = list_to_df(new_list1, headers=headers1)
-    df2 = list_to_df(new_list2, headers=headers2)
-
-    simuran.set_plot_style()
-
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df1, x="Time (s)", y="STA", ax=ax, style="Group", hue="Group")
-    mc = UnicodeGrabber.get("micro")
-    ax.set_ylabel(f"Spike triggered average {mc}V")
-    name = "average_sta_sub"
-    fig.savefig(os.path.join(out_dir, name + "." + img_format))
-    plt.close(fig)
-
-    fig, ax = plt.subplots()
-    sns.lineplot(
-        data=df2, x="Frequency (Hz)", y="SFC", ax=ax, style="Group", hue="Group"
+    here = os.path.dirname(os.path.abspath(__file__))
+    cell_list_location = os.path.join(
+        here, "..", "cell_lists", "CTRL_Lesion_cells_filled_eeg.xlsx"
     )
-    ax.set_ylabel(f"Spike field coherence")
-    name = "average_sfc_sub"
-    fig.savefig(os.path.join(out_dir, name + "." + img_format))
-    plt.close(fig)
+    df = pd.read_excel(cell_list_location)
 
-    out_fname = os.path.join(out_dir, base + "__sta" + ext)
-    df1.to_excel(out_fname, index=False)
-    out_fname = os.path.join(out_dir, base + "__sfc" + ext)
-    df2.to_excel(out_fname, index=False)
+    # later grab the cell type
+    print(info.columns)
+
+    for out_region in ["sub", "rsc"]:
+        for row in info.itertuples():
+            dir_ = row.Directory[len(base_dir + os.sep) :]
+            group = dir_[0]
+            animal_name = dir_.split(os.sep)[0].split("_")[0]
+            number = int(animal_name[-1])
+
+            if group == "C":
+                group = "Control"
+            elif group == "L":
+                group = "ATNx (Lesion)"
+            else:
+                raise ValueError("unsupported group {}".format(group))
+            if out_region == "sub":
+                sta = row.STA_SUB
+                sfc = row.SFC_SUB
+            else:
+                sta = row.STA_RSC
+                sfc = row.SFC_RSC
+            t = row.Time
+            f = row.Frequency
+
+            spatial = (
+                df[
+                    (df["Filename"] == row.Filename)
+                    & (df["Group"] == row.Group)
+                    & (df["Unit"] == row.Unit)
+                ]["class"]
+                .values.flatten()[0]
+                .split("_")[0]
+            )
+
+            for i in range(len(sta)):
+                new_list1.append([group, float(sta[i]), float(t[i]), spatial])
+            for i in range(len(sfc)):
+                new_list2.append([group, float(sfc[i]) / 100, float(f[i]), spatial])
+
+        headers1 = ["Group", "STA", "Time (s)", "Spatial"]
+        headers2 = ["Group", "SFC", "Frequency (Hz)", "Spatial"]
+        df1 = list_to_df(new_list1, headers=headers1)
+        df2 = list_to_df(new_list2, headers=headers2)
+
+        simuran.set_plot_style()
+
+        fig, ax = plt.subplots()
+        sns.lineplot(
+            data=df1, x="Time (s)", y="STA", ax=ax, style="Group", hue="Spatial"
+        )
+        mc = UnicodeGrabber.get("micro")
+        ax.set_ylabel(f"Spike triggered average {mc}V")
+        name = f"average_sta_{out_region}"
+        fig.savefig(os.path.join(out_dir, name + "." + img_format))
+        plt.close(fig)
+
+        fig, ax = plt.subplots()
+        sns.lineplot(
+            data=df2, x="Frequency (Hz)", y="SFC", ax=ax, style="Group", hue="Spatial"
+        )
+        ax.set_ylabel("Spike field coherence")
+        name = f"average_sfc_{out_region}"
+        fig.savefig(os.path.join(out_dir, name + "." + img_format))
+        plt.close(fig)
+
+        out_fname = os.path.join(out_dir, base + f"__sta_{out_region}" + ext)
+        df1.to_excel(out_fname, index=False)
+        out_fname = os.path.join(out_dir, base + f"__sfc_{out_region}" + ext)
+        df2.to_excel(out_fname, index=False)
