5c4b1ff8c0c21d59b12da0b62d57fbd1301671a1
Sean Martin <seankieran.m@hotmail.com>
2021-07-20 15:01:24 +0100

    Update config method

---
+++ lfp_atn_simuran/configs/default.py
@@ -0,0 +1,25 @@
+clean_kwargs = {
+    "pick_property": "group",
+    "channels": ["LFP"],
+}
+
+params = {
+    # Cleaning params
+    "clean_method": "pick",
+    "clean_kwargs": clean_kwargs,
+    
+    # Filtering params
+    "fmin": 1,
+    "fmax": 100,
+    "theta_min": 6,
+    "theta_max": 10,
+    "delta_min": 1.5,
+    "delta_max": 4,
+
+    # Plotting params
+    "psd_scale": "decibels",
+    "image_format": "png",
+
+    # Path setup
+    "cfg_base_dir" : r"D:\SubRet_recordings_imaging"
+}
