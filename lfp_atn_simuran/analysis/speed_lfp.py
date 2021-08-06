5365269bce6a885da6a58571607c6c8321f55d0c
Sean Martin <seankieran.m@hotmail.com>
2021-08-06 13:26:39 +0100

    Update speed lfp

--- lfp_atn_simuran/analysis/speed_lfp.py
+++ lfp_atn_simuran/analysis/speed_lfp.py
@@ -1,16 +1,18 @@
-import logging
-from copy import deepcopy
 from math import floor, ceil
 import os
 
 from neurochat.nc_utils import butter_filter
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.integrate._ivp.radau import T
 import seaborn as sns
 from skm_pyutils.py_table import list_to_df
 from skm_pyutils.py_plot import UnicodeGrabber
+from skm_pyutils.py_array import non_zero_divide
 import simuran
 import pandas as pd
+import scipy.signal
+import scipy.integrate
 
 from lfp_atn_simuran.analysis.lfp_clean import LFPClean
 
@@ -19,29 +21,31 @@ from lfp_atn_simuran.analysis.lfp_clean import LFPClean
 def speed_vs_amp(self, lfp_signal, low_f, high_f, filter_kwargs=None, **kwargs):
     """Self represents an nc_spatial object."""
     lim = kwargs.get("range", [0, self.get_duration()])
-    samples_per_sec = kwargs.get("samplesPerSec", 10)
+    samples_per_sec = kwargs.get("samplesPerSec", 5)
     do_once = True
 
-    if filter_kwargs is None:
-        filter_kwargs = {}
-    try:
-        lfp_signal = lfp_signal.filter(low_f, high_f, **filter_kwargs)
-    except BaseException:
-        lfp_signal = deepcopy(lfp_signal)
-        _filt = [10, low_f, high_f, "bandpass"]
-        lfp_signal._set_samples(
-            butter_filter(
-                lfp_signal.get_samples(), lfp_signal.get_sampling_rate(), *_filt
-            )
-        )
+    # Not filtering anymore since using relative power
+    # if filter_kwargs is None:
+    #     filter_kwargs = {}
+    # try:
+    #     lfp_signal = lfp_signal.filter(low_f, high_f, **filter_kwargs)
+    # except BaseException:
+    #     lfp_signal = deepcopy(lfp_signal)
+    #     _filt = [10, low_f, high_f, "bandpass"]
+    #     lfp_signal._set_samples(
+    #         butter_filter(
+    #             lfp_signal.get_samples(), lfp_signal.get_sampling_rate(), *_filt
+    #         )
+    #     )
 
     # Calculate the LFP power
     skip_rate = int(self.get_sampling_rate() / samples_per_sec)
     slicer = slice(skip_rate, -skip_rate, skip_rate)
     index_to_grab = np.logical_and(self.get_time() >= lim[0], self.get_time() <= lim[1])
     time_to_use = self.get_time()[index_to_grab][slicer]
-    speed = self.get_speed()[index_to_grab][slicer]
+    speed = self.get_speed()
 
+    avg_speed = np.zeros_like(time_to_use)
     lfp_amplitudes = np.zeros_like(time_to_use)
     lfp_samples = lfp_signal.get_samples()
     if hasattr(lfp_samples, "unit"):
@@ -53,12 +57,28 @@ def speed_vs_amp(self, lfp_signal, low_f, high_f, filter_kwargs=None, **kwargs):
 
     for i, t in enumerate(time_to_use):
         diff = 1 / (2 * samples_per_sec)
+
+        low_sample = floor((t - diff) * self.get_sampling_rate())
+        high_sample = floor((t + diff) * self.get_sampling_rate())
+        avg_speed[i] = np.mean(speed[low_sample:high_sample])
+
         low_sample = floor((t - diff) * lfp_signal.get_sampling_rate())
         high_sample = ceil((t + diff) * lfp_signal.get_sampling_rate())
         if high_sample < len(lfp_samples):
-            lfp_amplitudes[i] = np.mean(
-                np.abs(lfp_samples[low_sample : high_sample + 1])
+            lfp_sample_200ms = lfp_samples[low_sample : high_sample + 1]
+            slep_win = scipy.signal.hann(lfp_sample_200ms.size, False)
+            f, psd = scipy.signal.welch(
+                lfp_sample_200ms,
+                fs=lfp_signal.get_sampling_rate(),
+                window=slep_win,
+                nperseg=len(lfp_sample_200ms),
+                nfft=256,
+                noverlap=0,
             )
+            idx_band = np.logical_and(f >= low_f, f <= high_f)
+            abs_power = scipy.integrate.simps(psd[idx_band], x=f[idx_band])
+            total_power = scipy.integrate.simps(psd, x=f)
+            lfp_amplitudes[i] = abs_power / total_power
         elif do_once:
             simuran.log.warning(
                 "Position data ({}s) is longer than EEG data ({}s)".format(
@@ -67,27 +87,94 @@ def speed_vs_amp(self, lfp_signal, low_f, high_f, filter_kwargs=None, **kwargs):
             )
             do_once = False
 
-    # binsize = kwargs.get("binsize", 2)
-    # min_speed, max_speed = kwargs.get("range", [0, 40])
+    min_speed, max_speed = kwargs.get("SpeedRange", [0, 40])
+    # This kind of logic can be used in general to bin the speeds
+    # binsize = kwargs.get("SpeedBinSize", 2)
 
-    # max_speed = min(max_speed, np.ceil(speed.max() / binsize) * binsize)
-    # min_speed = max(min_speed, np.floor(speed.min() / binsize) * binsize)
+    # max_speed = min(max_speed, np.ceil(avg_speed.max()))
+    # min_speed = max(min_speed, np.floor(avg_speed.min()))
     # bins = np.arange(min_speed, max_speed, binsize)
 
-    # visit_time = np.histogram(speed, bins)[0]
-    # speedInd = np.digitize(speed, bins) - 1
+    # visit_time = np.histogram(avg_speed, bins)[0]
+    # speedInd = np.digitize(avg_speed, bins) - 1
 
-    # visit_time = visit_time / samples_per_sec
     # binned_lfp = [np.sum(lfp_amplitudes[speedInd == i]) for i in range(len(bins) - 1)]
-    # rate = np.array(binned_lfp) / visit_time
+    # rate = non_zero_divide(np.array(binned_lfp), visit_time)
+    # fig, ax = plt.subplots()
+    # ax.plot(bins[:-1], rate)
+    # plt.show()
+
+    spectrogram_len = min(
+        kwargs.get("SpectrogramLen", 150),
+        len(lfp_samples) / lfp_signal.get_sampling_rate(),
+    )
+    high_sample = floor(spectrogram_len * lfp_signal.get_sampling_rate())
+    high_speed_sample = floor(spectrogram_len * samples_per_sec)
+    lfp_sample_to_use = lfp_samples[:high_sample]
+    f, t, Sxx = scipy.signal.spectrogram(
+        x=lfp_sample_to_use,
+        fs=lfp_signal.get_sampling_rate(),
+        nperseg=(lfp_signal.get_sampling_rate() // 2),
+        nfft=512,
+    )
+    Sxx = 10 * np.log10(Sxx / np.amax(Sxx))
+    Sxx = Sxx.flatten()
+    Sxx[np.nonzero(Sxx < -40)] = -40
+    Sxx = np.reshape(Sxx, [f.size, t.size])
+    fig, ax = plt.subplots(figsize=(16, 10))
+
+    c_map = "magma"
+    levels = 22
+    dx = np.mean(np.diff(t))
+    dy = np.mean(np.diff(f))
+    pad_map = np.pad(Sxx[:-1, :-1], ((1, 1), (1, 1)), "edge")
+    vmin, vmax = np.nanmin(pad_map), np.nanmax(pad_map)
+    if vmax - vmin > 0.1:
+        splits = np.linspace(vmin, vmax, levels + 1)
+    else:
+        splits = np.linspace(vmin, vmin + 0.1 * levels, levels + 1)
+    splits = np.around(splits, decimals=1)
+    to_delete = []
+    for i in range(len(splits) - 1):
+        if splits[i] >= splits[i + 1]:
+            to_delete.append(i)
+    splits = np.delete(splits, to_delete)
+    x_edges = np.append(t - dx / 2, t[-1] + dx / 2)
+    y_edges = np.append(f - dy / 2, f[-1] + dy / 2)
+    pcm = ax.contourf(
+        x_edges, y_edges, pad_map, levels=splits, cmap=c_map, corner_mask=True
+    )
+
+    # pcm = ax.pcolormesh(
+    #     t, f, Sxx, cmap=c_map, edgecolors="none", rasterized=True, shading="auto"
+    # )
+    ax.set_xlim(t.min(), t.max())
+    ax.set_ylim(0, 30)
+
+    plt.colorbar(pcm, ax=ax, use_gridspec=True)
+
+    ax.set_xlabel("Time (sec)")
+    ax.set_ylabel("Frequency (Hz)")
+
+    # ax2 = ax.twinx()
+    ax.plot(
+        time_to_use[:high_speed_sample],
+        0.25 * avg_speed[:high_speed_sample],
+        c="k",
+        alpha=1,
+        linewidth=3,
+    )
+    # ax2.set_ylabel("Speed (cm / s)")
+    # ax2.set_ylim(0, np.max(avg_speed))
+    simuran.despine()
 
     pd_df = list_to_df(
-        [speed, lfp_amplitudes], transpose=True, headers=["Speed", "LFP amplitude"]
+        [avg_speed, lfp_amplitudes], transpose=True, headers=["Speed", "LFP amplitude"]
     )
-    pd_df = pd_df[pd_df["Speed"] <= 40]
+    pd_df = pd_df[pd_df["Speed"] <= max_speed]
     pd_df["Speed"] = np.around(pd_df["Speed"])
 
-    return pd_df
+    return pd_df, fig
 
 
 def speed_lfp_amp(
@@ -126,14 +213,20 @@ def speed_lfp_amp(
         lfp_signal = signal
 
         # Speed vs LFP power
-        pd_df = speed_vs_amp(spatial, lfp_signal, fmin, fmax, samplesPerSec=speed_sr)
+        pd_df, sfig = speed_vs_amp(
+            spatial, lfp_signal, fmin, fmax, samplesPerSec=speed_sr
+        )
+
+        out_name = basename + "_speed_theta_spectogram_{}".format(name)
+        sfig = simuran.SimuranFigure(sfig, out_name, dpi=400, done=True, format=fmt)
+        figures.append(sfig)
 
         results[f"{name}_df"] = pd_df
 
         fig, ax = plt.subplots()
         sns.lineplot(data=pd_df, x="Speed", y="LFP amplitude", ax=ax)
         simuran.despine()
-        fname = basename + "_speed_theta_{}".format(name)
+        fname = basename + "_speed_theta_corr_{}".format(name)
         speed_amp_fig = simuran.SimuranFigure(
             fig, filename=fname, done=True, format=fmt, dpi=400
         )
