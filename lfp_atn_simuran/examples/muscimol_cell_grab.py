7c4f50d757ff471dc7e33af83cbf7c9cfb1af46e
Sean Martin <seankieran.m@hotmail.com>
2021-08-25 17:36:34 +0100

    Finished grabbing list of cells

--- lfp_atn_simuran/examples/muscimol_cell_grab.py
+++ lfp_atn_simuran/examples/muscimol_cell_grab.py
@@ -2,7 +2,7 @@ import os
 
 import simuran
 from skm_pyutils.py_path import get_all_files_in_dir
-from skm_pyutils.py_table import list_to_df
+from skm_pyutils.py_table import list_to_df, df_to_file, df_from_file
 
 def main(start_dir, map_file):
 
@@ -11,7 +11,6 @@ def main(start_dir, map_file):
     #     comp = x.source_file[len(in_dir) + 2 :]
     #     return comp
 
-    # here = os.path.abspath(os.path.dirname(__file__))
     # mapping_dir = os.path.join(here, "..", "recording_mappings")
 
     # df = simuran.index_ephys_files(
@@ -26,9 +25,27 @@ def main(start_dir, map_file):
     # container.sort(sorting_fn)
     # container.print_units(f=os.path.join(start_dir, "full_cell_list.txt"))
 
+    here = os.path.abspath(os.path.dirname(__file__))
+    out_loc = os.path.join(here, "..", "cell_lists", "musc_cells_grab.csv")
     # cells_to_analyse = container.select_cells()
-    cells_to_analyse = simuran.dir_to_table(start_dir)
-    cells_to_analyse.to_csv(os.path.join(start_dir, "musc_cells_grab.csv"))
+    if not os.path.exists(out_loc):
+        cells_to_analyse = simuran.dir_to_table(start_dir)
+    else:
+        cells_to_analyse = df_from_file(out_loc)
+
+    # Fill in the mappings
+    mapping_dict = {
+        "CanCSCa1": "CanCSCa",
+        "CanCSR7": "CanCSR",
+        "CanCSR8": "CanCSR",
+    }
+    def mapping_from_row(row):
+        animal_name = row["Directory"][len(start_dir+os.sep):].split("_")[0]
+        return mapping_dict[animal_name]
+
+    cells_to_analyse["Mapping"] = cells_to_analyse.apply(mapping_from_row, axis=1)
+
+    df_to_file(cells_to_analyse, out_loc)
 
 if __name__ == "__main__":
     main_start_dir = (
