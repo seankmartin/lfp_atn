47a57b67d126a7e7ea0292f321b032b4c4a82e31
Sean Martin <seankieran.m@hotmail.com>
2021-09-01 14:08:40 +0100

    Fix temp msg

--- lfp_atn_simuran/recording_mappings/CL-SR_4-6-no-cells.py
+++ lfp_atn_simuran/recording_mappings/CL-SR_4-6-no-cells.py
@@ -7,7 +7,6 @@ This should contain information about the recording,
 such as the number electrodes in the recording.
 """
 
-# TODO this is a temp fix for some recordings not having all LFP
 def setup_signals():
     """Set up the signals (such as eeg or lfp)."""
 
